a8f1ae2de1470b0b571e5fffab82117136341f05
manishSRM <manishchaurasiya101@gmail.com>
2017-07-30 00:46:59 +0530

    Codebrahma second commit

--- codebramha challenge/script.js
+++ codebramha challenge/script.js
@@ -4,69 +4,85 @@ var myGrid = makeGrid(row, col);
 var max = (row*col) / 3;
 var min = 4;
 var villains = Math.floor(Math.random() * (max - min + 1)) + min;
-// E = empty V = Villian H = Hero
+// E = Empty V = Villian H = Hero
 var statusGrid = generateStatusGrid(row, col);
-locateVilliansInGrid(villains, row, col, statusGrid, myGrid);
+var villianCellList = locateVilliansInGrid(villains, row, col, statusGrid, myGrid);
+var heroLocation = locateOurHeroInGrid(statusGrid, myGrid, row, col);
+// setInterval(tryToMove(heroLocation), 1000);
 
-function isThisCellEmpty(cellForVillian, statusGrid) {
-    return statusGrid[cellForVillian[0]][cellForVillian[1]] == 'E' ? true : false;
+function tryToMove(heroLocation) {
+    var destination = [heroLocation[0]-1, heroLocation[1]-1];
+    var thisCanvas = document.getElementById(heroLocation[0].toString() + heroLocation[1].toString());
+    var thisContext = thisCanvas.getContext('2d');
+    var x = thisCanvas.width;
+    var y = thisCanvas.height;
+    thisContext.clearRect(0, 0, x, y);
+    thisContext.beginPath();
+    thisContext.arc(21, 8, 7, 0, 2*Math.PI);
+    thisContext.fillStyle = "blue";
+    thisContext.fill();
+    thisContext.stroke();
+
 }
 
-function generateNewCellForVillian(row, col) {
+function isThisCellEmpty(newCell, statusGrid) {
+    return statusGrid[newCell[0]][newCell[1]] == 'E' ? true : false;
+}
+
+function generateNewCell(row, col) {
     var cell_X = Math.floor(Math.random() * (row));
     var cell_Y = Math.floor(Math.random() * (col));
     if (cell_X < 0 || cell_Y < 0)
-        return generateNewCellForVillian(row, col)
+        return generateNewCell(row, col)
     return [cell_X, cell_Y];
 }
 
-function locateInthisCell(cellForVillian, statusGrid, myGrid) {
-    statusGrid[cellForVillian[0]][cellForVillian[1]] = "V";
+function locateInthisCell(cell, statusGrid, myGrid, character) {
+    statusGrid[cell[0]][cell[1]] = character == "H" ? "H" : "V";
     var trs = myGrid.getElementsByTagName('tr');
-    var tds = trs[cellForVillian[0]].getElementsByTagName('td');
+    var tds = trs[cell[0]].getElementsByTagName('td');
     var thisCanvas = document.createElement("canvas");
+    thisCanvas.id = cell[0].toString() + cell[1].toString();
     thisCanvas.width = 41;
     thisCanvas.height = 17;
     var thisContext = thisCanvas.getContext('2d');
     thisContext.beginPath();
     thisContext.arc(21, 8, 7, 0, 2*Math.PI);
-    thisContext.fillStyle = "red";
+    thisContext.fillStyle = (character == "H") ? "blue" : "red";
     thisContext.fill();
     thisContext.stroke();
-    tds[cellForVillian[1]].appendChild(thisCanvas);
+    tds[cell[1]].appendChild(thisCanvas);
 }
 
 function locateVilliansInGrid(villains, row, col, statusGrid, myGrid) {
+    var villianCellList = [];
     for (var i = 0; i < villains; i++) {
-        var cellForVillian = generateNewCellForVillian(row, col);
+        var cellForVillian = generateNewCell(row, col);
         cellForVillian[0] = parseInt(cellForVillian[0]);
         cellForVillian[1] = parseInt(cellForVillian[1]);
         while (!isThisCellEmpty(cellForVillian, statusGrid)) {
-            cellForVillian = generateNewCellForVillian(row, col);
+            cellForVillian = generateNewCell(row, col);
             cellForVillian[0] = parseInt(cellForVillian[0]);
             cellForVillian[1] = parseInt(cellForVillian[1]);
         }
-        locateInthisCell(cellForVillian, statusGrid, myGrid);
+        villianCellList.push(cellForVillian);
+        locateInthisCell(cellForVillian, statusGrid, myGrid, "V");
     }
+    return villianCellList;
 }
 
-// var trs = myGrid.getElementsByTagName('tr');
-// for (var i = 0; i < trs.length; i++) {
-//     var tds = trs[i].getElementsByTagName("td");
-//     for (var j = 0; j < tds.length; j++) {
-//         var thisCanvas = document.createElement("canvas");
-//         thisCanvas.width = 41;
-//         thisCanvas.height = 17;
-//         var thisContext = thisCanvas.getContext('2d');
-//         thisContext.beginPath();
-//         thisContext.arc(21, 8, 7, 0, 2*Math.PI);
-//         thisContext.fillStyle = "red";
-//         thisContext.fill();
-//         thisContext.stroke();
-//         tds[j].appendChild(thisCanvas);
-//         // console.log(tds[j]);
-//     }
-// }
+function locateOurHeroInGrid(statusGrid, myGrid, row, col) {
+    var cellForHero = undefined;
+    while (true) {
+        cellForHero = generateNewCell(row, col);
+        cellForHero[0] = parseInt(cellForHero[0]);
+        cellForHero[1] = parseInt(cellForHero[1]);
+        if (isThisCellEmpty(cellForHero, statusGrid))
+            break;
+    }
+    locateInthisCell(cellForHero, statusGrid, myGrid, "H");
+    return cellForHero;
+}
 
 function makeGrid(row, col) {
     var grid = document.createElement("table");
